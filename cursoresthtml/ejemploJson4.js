c48e719831fee16794aa00ae32b32b545c0cab70
Prestamo Estudiantes <prestamour@urosario.edu.co>
2019-09-26 14:40:29 -0500

    actializacion tarde 3 pm

--- cursoresthtml/ejemploJson4.js
+++ cursoresthtml/ejemploJson4.js
@@ -3,25 +3,27 @@ var request= new XMLHttpRequest();
 request.open('GET', 
              'https://ghibliapi.herokuapp.com/films', true);
 request.onload=function() {
-  var data=JSON.parse(this.response);
+  var data = JSON.parse(this.response);
 
-  data.forEach(movie => {
-      console.log(movie.title);
-   })
-}
-request.send();
+data.forEach(movie => {
+    const card = document.createElement('div')
+    card.setAttribute('class', 'card')
+    const h1 = document.createElement('h1')
+    h1.textContent = movie.title
+    const p = document.createElement('p')
+    movie.description = movie.description.substring(0, 300) // Limit to 300 chars
+    p.textContent = movie.description;
+    container.appendChild(card);
+    card.appendChild(h1);
+    card.appendChild(p);
+    })
+  }
+    request.send();
 
-const app = document.getElementById('root');
-//console.log(app);
-const logo = document.createElement('img')
-//console.log(logo);
-logo.src = 'logo.png';
-console.log(logo);
-
-const container=document.createElement('div')
-container.setAttribute('class', 'container') //atributos a la etiqueta html
-//(crea el atributo class y le asigna el valor container que esta en la hoja de estilo)
-
-app.appendChild(logo)
-app.appendChild(container)
-
+    const app=document.getElementById('root');
+    const logo=document.createElement('img');
+    const container=document.createElement('div');
+    container.setAttribute('class', 'container');
+    logo.src='logo.png';
+    app.appendChild(logo);
+    app.appendChild(container);
